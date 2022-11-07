a6da3211487635856c129e1ceadb379c45367b50
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-11-07 18:47:24 +0530

    coding challenge

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// Coding Challenge 2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  header.addEventListener('click', () => (header.style.color = 'blue'));
+})();
+
+/*
 // More examples about closures
 
 let f;
